02d667c106e80ca4d07f5db46373afe5fd090d68
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:52:31 +0300

    Update BackupService.cs

--- Services/BackupService.cs
+++ Services/BackupService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿
 using Beckamo.Models;
 using Beckamo.Services;
@@ -84,70 +83,4 @@ public class BackupService : IBackupService
         }
     }
 }   
-=======
-﻿
-using Beckamo.Models;
-using Beckamo.Services;
-using Serilog;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace Beckamo.Services;
-
-public class BackupService : IBackupService
-{
-    private readonly ISourceDatabaseService _sourceDb;
-    private readonly ITargetStorageService _target;
-    private readonly IEncryptionService _encryption;
-    private readonly ILogger _logger;
-
-    public BackupService(
-        ISourceDatabaseService sourceDb,
-        ITargetStorageService target,
-        IEncryptionService encryption,
-        ILogger logger)
-    {
-        _sourceDb = sourceDb;
-        _target = target;
-        _encryption = encryption;
-        _logger = logger;
-    }
-
-    public async Task ExecuteBackupAsync(BackupSettings settings)
-    {
-        foreach (var dbName in settings.IncludedDatabases)
-        {
-            try
-            {
-                _logger.Information("Начинаем бекап базы {Database}", dbName);
-
-                var tables = await _sourceDb.GetTablesAsync(dbName, settings.IncludedTables);
-
-                foreach (var table in tables)
-                {
-                    _logger.Information("Экспортируем таблицу {Table} из базы {Database}", table, dbName);
-
-                    var data = await _sourceDb.ExportTableAsync(dbName, table);
-
-                    var encryptedPath = await _encryption.EncryptToFileAsync(
-                        data,
-                        table,
-                        settings.UseEncryption,
-                        settings.EncryptionPassword
-                    );
-
-                    var targetPath = _target.BuildPath(dbName, table, settings.TargetFilePathTemplate);
-                    await _target.MoveFileAsync(encryptedPath, targetPath);
-
-                    _logger.Information("Бекап таблицы {Table} сохранён в {Path}", table, targetPath);
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.Error(ex, "Ошибка бекапа базы {Database}", dbName);
-            }
-        }
-    }
-}
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
