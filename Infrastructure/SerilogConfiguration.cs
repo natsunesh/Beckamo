55e72cce9abb9c23a34201daec8930066f4bf939
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:54:19 +0300

    Update SerilogConfiguration.cs

--- Infrastructure/SerilogConfiguration.cs
+++ Infrastructure/SerilogConfiguration.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Serilog;
 using Serilog.Sinks.SystemConsole;
 using Serilog.Sinks.File;
@@ -15,22 +14,5 @@ public static class SerilogConfiguration
             .WriteTo.File("logs/backamo.log", rollingInterval: RollingInterval.Day)
             .CreateLogger();
     }
-=======
-﻿using Serilog;
-using Serilog.Sinks.SystemConsole;
-using Serilog.Sinks.File;
 
-namespace Beckamo.Infrastructure;
-
-public static class SerilogConfiguration
-{
-    public static void ConfigureSerilog()
-    {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .WriteTo.Console()
-            .WriteTo.File("logs/backamo.log", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
-    }
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
