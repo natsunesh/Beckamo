f92426ba7510b352aca5130db9d427e06ff907f9
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:53:51 +0300

    Update AesKeyProvider.cs

--- Infrastructure/Security/AesKeyProvider.cs
+++ Infrastructure/Security/AesKeyProvider.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Security.Cryptography;
 using System.Text;
 
@@ -15,22 +14,5 @@ public class AesKeyProvider
     }
 
    
-=======
-﻿using System.Security.Cryptography;
-using System.Text;
 
-namespace Beckamo.Infrastructure.Security;
-
-public class AesKeyProvider
-{
-    public static byte[] GetKeyFromPassword(string password, string salt)
-    {
-        return new Rfc2898DeriveBytes(
-            password,
-            Encoding.UTF8.GetBytes(salt))
-            .GetBytes(32);
-    }
-
-   
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
