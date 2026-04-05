10ea94b2c0a6ffc79b05ef1ccb91178818b40792
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:54:09 +0300

    Update ServiceCollectionExtensions.cs

--- Infrastructure/Security/ServiceCollectionExtensions.cs
+++ Infrastructure/Security/ServiceCollectionExtensions.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Beckamo.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,24 +16,5 @@ public static class ServiceCollectionExtensions
 
        
     }
-=======
-﻿using Beckamo.Services;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace Beckamo.Infrastructure.Security;
-public static class ServiceCollectionExtensions
-{
-    public static void ConfigureServices(this IServiceCollection services)
-    {
-        // Сервисы
-        services
-            .AddSingleton<IBackupService, BackupService>()
-            .AddSingleton<IEncryptionService, AesEncryptionService>()
-            .AddSingleton<ISourceDatabaseService, PgSourceDatabaseService>()
-            .AddSingleton<ITargetStorageService, LocalTargetStorageService>()
-            .AddSingleton<IErrorHandler, BackupErrorHandler>();
-
-       
-    }
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
