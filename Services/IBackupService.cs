31911a1f06706e3cec66a8589fda53648bc755ea
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:52:12 +0300

    Update IBackupService.cs

--- Services/IBackupService.cs
+++ Services/IBackupService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Threading.Tasks;
 using Beckamo.Models;
 
@@ -7,14 +6,5 @@ namespace Beckamo.Services;
 public interface IBackupService
 {
     Task ExecuteBackupAsync(BackupSettings settings);
-=======
-﻿using System.Threading.Tasks;
-using Beckamo.Models;
 
-namespace Beckamo.Services;
-
-public interface IBackupService
-{
-    Task ExecuteBackupAsync(BackupSettings settings);
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
