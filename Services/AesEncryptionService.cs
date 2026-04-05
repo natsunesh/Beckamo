d9317427537ab759c1280c91ba7e13e587b7363f
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:53:04 +0300

    Update AesEncryptionService.cs

--- Services/AesEncryptionService.cs
+++ Services/AesEncryptionService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using BCrypt.Net;
 using Beckamo.Services;
 using Serilog;
@@ -53,60 +52,5 @@ public class AesEncryptionService : IEncryptionService
         _logger.Information("Файл {Table} зашифрован: {Path}, ключ хеширован через BCrypt.", tableName, tempPath);
         return tempPath;
     }
-=======
-﻿using BCrypt.Net;
-using Beckamo.Services;
-using Serilog;
-using System;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 
-namespace Beckamo.Services;
-public class AesEncryptionService : IEncryptionService
-{
-    private readonly ILogger _logger;
-
-    public AesEncryptionService(ILogger logger)
-    {
-        _logger = logger;
-    }
-
-    public async Task<string> EncryptToFileAsync(
-        byte[] data,
-        string tableName,
-        bool useEncryption,
-        string password)
-    {
-        var tempPath = Path.GetTempFileName();
-
-        if (!useEncryption)
-        {
-            // Просто запись в файл без шифрования
-            await File.WriteAllBytesAsync(tempPath, data);
-            _logger.Information("Файл {Table} записан без шифрования: {Path}", tableName, tempPath);
-            return tempPath;
-        }
-
-        // BCrypt хеширует пароль → ключ для AES‑256
-        var salt = BCrypt.Net.BCrypt.GenerateSalt();
-        var hashedKey = BCrypt.Net.BCrypt.HashPassword(password, salt);
-
-        // Берём первые 32 байта хешированного ключа как AES‑256 ключ
-        var keyBytes = new Rfc2898DeriveBytes(hashedKey, Encoding.UTF8.GetBytes(salt)).GetBytes(32);
-            
-
-        using var aes = Aes.Create();
-        aes.Key = keyBytes;
-        aes.GenerateIV();
-
-        using var fileStream = new FileStream(tempPath, FileMode.Create);
-        using var cryptoStream = new CryptoStream(fileStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
-        await cryptoStream.WriteAsync(data, 0, data.Length);
-        cryptoStream.FlushFinalBlock();
-
-        _logger.Information("Файл {Table} зашифрован: {Path}, ключ хеширован через BCrypt.", tableName, tempPath);
-        return tempPath;
-    }
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
