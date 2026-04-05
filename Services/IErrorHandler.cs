2cacfbae3a14a023506ecda0abd4ab71a761888a
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:51:47 +0300

    Update IErrorHandler.cs

--- Services/IErrorHandler.cs
+++ Services/IErrorHandler.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Threading.Tasks;
 namespace Beckamo.Services;
@@ -6,13 +5,5 @@ namespace Beckamo.Services;
 public interface IErrorHandler
 {
     Task HandleAsync(Exception ex, string databaseName);
-=======
-﻿using System;
-using System.Threading.Tasks;
-namespace Beckamo.Services;
 
-public interface IErrorHandler
-{
-    Task HandleAsync(Exception ex, string databaseName);
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
