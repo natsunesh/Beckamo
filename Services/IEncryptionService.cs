9b66c916d9f75b1345892c89072e7829e838ac9c
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:52:01 +0300

    Update IEncryptionService.cs

--- Services/IEncryptionService.cs
+++ Services/IEncryptionService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Threading.Tasks;
 
 namespace Beckamo.Services;
@@ -9,16 +8,5 @@ public interface IEncryptionService
         string tableName,
         bool useEncryption,
         string password);
-=======
-﻿using System.Threading.Tasks;
 
-namespace Beckamo.Services;
-public interface IEncryptionService
-{
-    Task<string> EncryptToFileAsync(
-        byte[] data,
-        string tableName,
-        bool useEncryption,
-        string password);
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
