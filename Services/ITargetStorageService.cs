5fc7038a72a88a880d9f57509f4ee462fa55ab44
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:51:14 +0300

    Update ITargetStorageService.cs

--- Services/ITargetStorageService.cs
+++ Services/ITargetStorageService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Threading.Tasks;
 
 namespace Beckamo.Services;
@@ -6,13 +5,5 @@ public interface ITargetStorageService
 {
     string BuildPath(string databaseName, string tableName, string template);
     Task MoveFileAsync(string sourcePath, string targetPath);
-=======
-﻿using System.Threading.Tasks;
 
-namespace Beckamo.Services;
-public interface ITargetStorageService
-{
-    string BuildPath(string databaseName, string tableName, string template);
-    Task MoveFileAsync(string sourcePath, string targetPath);
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
