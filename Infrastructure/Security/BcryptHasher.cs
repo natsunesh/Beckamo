61454742d514dcaf0c1bcd5ec86331fdeae45671
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:54:00 +0300

    Update BcryptHasher.cs

--- Infrastructure/Security/BcryptHasher.cs
+++ Infrastructure/Security/BcryptHasher.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using BCrypt.Net;
 
 namespace Beckamo.Infrastructure.Security;
@@ -14,21 +13,5 @@ public static class BcryptHasher
     {
         return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
     }
-=======
-﻿using BCrypt.Net;
 
-namespace Beckamo.Infrastructure.Security;
-
-public static class BcryptHasher
-{
-    public static string Hash(string password)
-    {
-        return BCrypt.Net.BCrypt.HashPassword(password, 12);
-    }
-
-    public static bool Verify(string password, string hashedPassword)
-    {
-        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-    }
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
