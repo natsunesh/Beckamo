8b6a03edb7cb57087c2e2da75785c8696b11fb32
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:51:37 +0300

    Update ISourceDatabaseService.cs

--- Services/ISourceDatabaseService.cs
+++ Services/ISourceDatabaseService.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,15 +9,5 @@ public interface ISourceDatabaseService
     Task<List<string>> GetTablesAsync(string databaseName, List<string> includedTables);
     Task<byte[]> ExportTableAsync(string databaseName, string tableName);
     Task<string> ExportTableDataToCopyAsync(string databaseName, string tableName, string dumpFilePath);
-=======
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace Beckamo.Services;
-
-public interface ISourceDatabaseService
-{
-    Task<List<string>> GetTablesAsync(string databaseName, List<string> includedTables);
-    Task<byte[]> ExportTableAsync(string databaseName, string tableName);
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
 }
