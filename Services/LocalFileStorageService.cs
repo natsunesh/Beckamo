e93cb8f619da3176d550bd5208d380ad8d167fc3
natsunesh <101209597+natsunesh@users.noreply.github.com>
2026-04-05 12:50:58 +0300

    Update LocalFileStorageService.cs

--- Services/LocalFileStorageService.cs
+++ Services/LocalFileStorageService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Beckamo.Services;
 using Serilog;
 using System;
@@ -44,51 +43,3 @@ public class LocalTargetStorageService : ITargetStorageService
             _logger.Warning("Файл не найден для переноса: {source}", sourcePath);
         }
     }
-=======
-﻿using Beckamo.Services;
-using Serilog;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-
-namespace Beckamo.Services;
-
-public class LocalTargetStorageService : ITargetStorageService
-{
-    private readonly ILogger _logger;
-
-    public LocalTargetStorageService(ILogger logger)
-    {
-        _logger = logger;
-    }
-
-    public string BuildPath(string databaseName, string tableName, string template)
-    {
-        var fileName = template
-            .Replace("{db}", databaseName)
-            .Replace("{table}", tableName)
-            .Replace("{timestamp}", DateTime.UtcNow.ToString("yyyyMMdd_HHmmss"));
-
-        var dir = Path.GetDirectoryName(fileName);
-        if (!Directory.Exists(dir))
-            Directory.CreateDirectory(dir);
-
-        return fileName;
-    }
-
-    public async Task MoveFileAsync(string sourcePath, string targetPath)
-    {
-        if (File.Exists(sourcePath))
-        {
-            await using var source = File.OpenRead(sourcePath);
-            await using var dest = File.Create(targetPath);
-            await source.CopyToAsync(dest);
-            _logger.Information("Файл перенесён: {source} -> {target}", sourcePath, targetPath);
-        }
-        else
-        {
-            _logger.Warning("Файл не найден для переноса: {source}", sourcePath);
-        }
-    }
->>>>>>> 4cd8a9c83d127e7b68b0c141b233ba7e0124c7bb
-}
